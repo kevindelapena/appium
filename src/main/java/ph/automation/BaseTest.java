5d545c7cd6cc16a77757018c89974b84d598e9dc
kevin <kevindelapena@gmail.com>
2024-05-03 16:01:15 +0800

    Appium Gestures

--- src/main/java/ph/automation/BaseTest.java
+++ src/main/java/ph/automation/BaseTest.java
@@ -1,9 +1,13 @@
 package ph.automation;
 
+import com.google.common.collect.ImmutableMap;
 import io.appium.java_client.android.AndroidDriver;
 import io.appium.java_client.android.options.UiAutomator2Options;
 import io.appium.java_client.service.local.AppiumDriverLocalService;
 import io.appium.java_client.service.local.AppiumServiceBuilder;
+import org.openqa.selenium.JavascriptExecutor;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.remote.RemoteWebElement;
 import org.testng.annotations.AfterClass;
 import org.testng.annotations.BeforeClass;
 
@@ -33,7 +37,32 @@ public class BaseTest {
 
     }
 
+    public void longPressAction(WebElement ele){
+        ((JavascriptExecutor)driver).executeScript("mobile: longClickGesture",
+                ImmutableMap.of("elementId", ((RemoteWebElement) ele).getId(),
+                        "duration", 2000));
+    }
 
+    public void scrollToEndAction(){
+        boolean canScrollMore;
+        do{
+            canScrollMore = (Boolean) ((JavascriptExecutor) driver).executeScript("mobile: scrollGesture", ImmutableMap.of(
+                    "left", 100, "top", 100, "width", 200, "height", 200,
+                    "direction", "down",
+                    "percent", 3.0
+            ));
+        }while(canScrollMore);
+
+    }
+
+
+    public void swipeAction(WebElement ele, String direction){
+        ((JavascriptExecutor) driver).executeScript("mobile: swipeGesture", ImmutableMap.of(
+                "elementId", ((RemoteWebElement)ele).getId(),
+                "direction", direction,
+                "percent", 0.75
+        ));
+    }
     @AfterClass
     public void tearDown(){
         driver.quit();
---
+++ src/main/java/ph/automation/ScrollDemo.java
@@ -0,0 +1,17 @@
+package ph.automation;
+
+import io.appium.java_client.AppiumBy;
+import org.testng.annotations.Test;
+
+public class ScrollDemo extends BaseTest {
+
+    @Test
+    public void ScrollDemoTest() throws InterruptedException {
+        driver.findElement(AppiumBy.accessibilityId("Views")).click();
+//        driver.findElement(AppiumBy.androidUIAutomator("new UiScrollable(new UiSelector()).scrollIntoView(text(\"WebView\"));"));
+        Thread.sleep(2000);
+
+        scrollToEndAction();
+
+    }
+}
